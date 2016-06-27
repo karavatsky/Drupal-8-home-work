aca99e06633379364f1544b81d256b9709bfedc1
karavatski <ihar_karavatski@epam.com>
2016-06-27 08:31:29 +0300

    Settings

---
+++ docroot/sites/default/settings.local.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: igor
+ * Date: 26.6.16
+ * Time: 9.28
+ */
+
+$databases['default']['default'] = array (
+  'database' => 'd8hw',
+  'username' => 'root',
+  'password' => '',
+  'host' => 'localhost',
+  'driver' => 'mysql',
+  'prefix' => '',
+);
+
+$settings['hash_salt'] = 'jkljkljlkjj345j345';
+$config_directories['sync'] = __DIR__ . '/sync';
