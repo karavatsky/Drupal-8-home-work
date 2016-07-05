82948e0a65c6592b24b82df26198ffac042c4d28
karavatski <ihar_karavatski@epam.com>
2016-07-05 08:45:57 +0300

    Demo currency module #2.

--- docroot/modules/custom/demo_currencies/src/NBRBCurrencies.php
+++ docroot/modules/custom/demo_currencies/src/NBRBCurrencies.php
@@ -29,7 +29,20 @@ class NBRBCurrencies {
    */
   public function __construct(ClientInterface $http_client, $baseUrl) {
     $this->httpClient = $http_client;
+    $this->baseUrl = $baseUrl;
+
+  public function getData() {
+    $request = $this->httpClient->createRequest($this->baseUrl);
+    $request->getQuery()
+      ->set('length', 10);
+
+//    $res = $this->httpClient->request('GET', $this->baseUrl, [
+//      'auth' => ['ondate', 'pass']
+//    ]);
   }
-  
+
+  protected function prepareData() {
+
+  }
 }
 
