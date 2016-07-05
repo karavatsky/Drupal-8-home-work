a4a4b70002c735b121957228db0857ddbee92e3b
Karavatsky <ihar_karavatski@epam.com>
2016-07-05 18:11:06 +0300

    Demo currency module #3.

--- docroot/modules/custom/demo_currencies/src/NBRBCurrencies.php
+++ docroot/modules/custom/demo_currencies/src/NBRBCurrencies.php
@@ -20,6 +20,10 @@ class NBRBCurrencies {
   protected $url;
 
   protected $baseUrl;
+
+  protected $date;
+
+  protected $data;
   
   /**
    * Constructs a NBRBCurrencies object.
@@ -30,19 +34,71 @@ class NBRBCurrencies {
   public function __construct(ClientInterface $http_client, $baseUrl) {
     $this->httpClient = $http_client;
     $this->baseUrl = $baseUrl;
-
-  public function getData() {
-    $request = $this->httpClient->createRequest($this->baseUrl);
-    $request->getQuery()
-      ->set('length', 10);
-
-//    $res = $this->httpClient->request('GET', $this->baseUrl, [
-//      'auth' => ['ondate', 'pass']
-//    ]);
   }
 
-  protected function prepareData() {
+  public function getData() {
+    $xml = $this->getXml();
+    return $this->parseXml($xml);
+  }
 
+  public function setDate($date) {
+    $this->date = $date;
+  }
+
+  /**
+   * Get date of currencies as xml.
+   * 
+   * @param $date
+   * @return string
+   */
+  protected function getXml() {
+    $url = $this->buildUrl($this->date);
+    $data = '';
+    try {
+      $data = (string) $this->httpClient
+        ->get($url)
+        ->getBody();
+    } catch (RequestException $exception) {
+      watchdog_exception('update', $exception);
+    }
+    return $data;
+  }
+
+
+  /**
+   * Generate URL to fetch information about currencies.
+   * 
+   * @param $date
+   *  Format - m/d/Y .
+   * @return string
+   *   The URL for fetching information about currencies.
+   */
+  protected function buildUrl() {
+    $url = $this->baseUrl;
+    if (!empty($this->date)) {
+      $url = $url . '/ondate=' . $this->date;
+    }
+    $this->url = $url;
+    return $url;
+  }
+
+  protected function parseXml($raw_xml) {
+    try {
+      $xml = new \SimpleXMLElement($raw_xml);
+    }
+    catch (\Exception $e) {
+      // SimpleXMLElement::__construct produces an E_WARNING error message for
+      // each error found in the XML data and throws an exception if errors
+      // were detected. Catch any exception and return failure (NULL).
+      return NULL;
+    }
+    // If there is no valid project data, the XML is invalid, so return failure.
+    if (!isset($xml->short_name)) {
+      return NULL;
+    }
+    $data = array();
+    $this->data = $data;
+    return $data;
   }
 }
 
